6fa8160ede5124e68d6704a17f3d948022346f5b
Dieter Halosta <dieter.halosta@yahoo.com>
2020-03-23 14:20:25 +0200

    Finished creating a paint calculator.

--- src/main/java/org/fasttrackit/calculations/PaintCalculator.java
+++ src/main/java/org/fasttrackit/calculations/PaintCalculator.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class PaintCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        final int SQUARE_FEET_PER_GALLON = 350;
 
         //User input
         System.out.println("What is the length of the room in feet?");
-        int ceilingLenght = scanner.nextInt();
+        int ceilingLength = scanner.nextInt();
         System.out.println("What is the width of the room in feet?");
-        int ceilingidth = scanner.nextInt();
+        int ceilingWidth = scanner.nextInt();
 
         //Calculations
-        int roomArea = ceilingLenght * ceilingidth;
-        //TODO: calculate the gallons needed.
+        double roomArea = ceilingLength * ceilingWidth;
+        double paintNeeded = roomArea / SQUARE_FEET_PER_GALLON;
+
+        double gallonsNeeded = Math.round(paintNeeded);
+
         //System output
-        System.out.println("You will need to purchase  gallons of paint to cover " + roomArea + " square feet.");
+        System.out.println("You will need to purchase " + gallonsNeeded + " gallons of paint to cover " + roomArea + " square feet.");
     }
 }
