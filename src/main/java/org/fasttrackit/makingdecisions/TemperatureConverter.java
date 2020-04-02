bd38b7270e018acb5196b913b5c7d6906c78e427
Dieter Halosta <dieter.halosta@yahoo.com>
2020-04-02 12:03:29 +0300

    Fixed method TemperatureConverter

--- src/main/java/org/fasttrackit/makingdecisions/TemperatureConverter.java
+++ src/main/java/org/fasttrackit/makingdecisions/TemperatureConverter.java
@@ -9,29 +9,44 @@ public class TemperatureConverter {
 
         System.out.println("Press C to convert from Fahrenheit to Celsius.\nPress F to convert from Celsius to Fahrenheit.");
         String userInput = scanner.nextLine();
+        final double FC = 5.0/9.0;
+        final double CF = 9.0/5.0;
 
-        if (userInput.equals("c")){
-           String userInputUpper = userInput.toUpperCase();
-            System.out.println("Your choice: " + userInputUpper + "\nPlease enter the temperature in Fahrenheit:");
-            int f = scanner.nextInt();
-            int c = ((f -32) * (5/9));
-            System.out.println("The temperature in Celsius is " + c);
-        } else if (userInput.equals("C")) {
-            System.out.println("Your choice: " + userInput + "\nPlease enter the temperature in Fahrenheit:");
-            int f = scanner.nextInt();
-            int c = ((f - 32) * (5/9));
-            System.out.println("The temperature in Celsius is " + c);
-        } else if (userInput.equals("f")) {
-            String userInputUpper = userInput.toUpperCase();
-            System.out.println("Your choice: " + userInputUpper + "\nPlease enter the temperature in Celsius:");
-            int c = scanner.nextInt();
-            int f = ((c * (9/5)) + 32 );
-            System.out.println("The temperature in Fahrenheit is " + f);
-        } else if (userInput.equals("F")) {
-            System.out.println("Your choice: " + userInput + "\nPlease enter the temperature in Celsius:");
-            int c = scanner.nextInt();
-            int f = ((c * (9/5)) + 32 );
-            System.out.println("The temperature in Fahrenheit is " + f);
+        switch (userInput) {
+            case "c": {
+                String userInputUpper = userInput.toUpperCase();
+                System.out.println("Your choice: " + userInputUpper + "\nPlease enter the temperature in Fahrenheit:");
+                double f = scanner.nextDouble();
+                double c = ((f - 32) * FC);
+                double roundOffC = (double) Math.round(c * 100) / 100;
+                System.out.println("The temperature in Celsius is " + roundOffC);
+                break;
+            }
+            case "C": {
+                System.out.println("Your choice: " + userInput + "\nPlease enter the temperature in Fahrenheit:");
+                double f = scanner.nextDouble();
+                double c = ((f - 32) * FC);
+                double roundOffC = (double) Math.round(c * 100) / 100;
+                System.out.println("The temperature in Celsius is " + roundOffC);
+                break;
+            }
+            case "f": {
+                String userInputUpper = userInput.toUpperCase();
+                System.out.println("Your choice: " + userInputUpper + "\nPlease enter the temperature in Celsius:");
+                double c = scanner.nextDouble();
+                double f = ((c * CF) + 32);
+                double roundOffF = (double) Math.round(f * 100) / 100;
+                System.out.println("The temperature in Fahrenheit is " + roundOffF);
+                break;
+            }
+            case "F": {
+                System.out.println("Your choice: " + userInput + "\nPlease enter the temperature in Celsius:");
+                double c = scanner.nextDouble();
+                double f = ((c * CF) + 32);
+                double roundOffF = (double) Math.round(f * 100) / 100;
+                System.out.println("The temperature in Fahrenheit is " + roundOffF);
+                break;
+            }
         }
 
     }
