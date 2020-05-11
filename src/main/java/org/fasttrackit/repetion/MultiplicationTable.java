acac4a6d2a1bf86410b95740df458415d14776ad
Dieter Halosta <dieter.halosta@yahoo.com>
2020-05-11 13:11:22 +0300

    Finished working on the MultiplicationTable exercise.

--- src/main/java/org/fasttrackit/repetion/MultiplicationTable.java
+++ src/main/java/org/fasttrackit/repetion/MultiplicationTable.java
@@ -1,4 +1,11 @@
 package org.fasttrackit.repetion;
 
 public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int i=0; i<=12; i++){
+            for (int j=0; j<=12; j++){
+                System.out.println(i + " x " + j + " = " + i*j );
+            }
+        }
+    }
 }
