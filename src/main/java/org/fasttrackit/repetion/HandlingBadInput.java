fe1908a43cedc500c110002731af3f6a39d23f77
Dieter Halosta <dieter.halosta@yahoo.com>
2020-05-11 13:07:28 +0300

    Finished working on the HandlingBadInput exercise.

--- src/main/java/org/fasttrackit/repetion/HandlingBadInput.java
+++ src/main/java/org/fasttrackit/repetion/HandlingBadInput.java
@@ -1,4 +1,39 @@
 package org.fasttrackit.repetion;
 
+import org.fasttrackit.utils.ScannerUtils;
+
+import java.util.InputMismatchException;
+
+
 public class HandlingBadInput {
+
+    public static void rateCalculator(){
+        System.out.println("What is the rate of return?");
+        int userRate = ScannerUtils.nextIntAndMoveToNextLine();
+
+        if(userRate <= 0){
+            System.out.println("Sorry. That's not a valid input.");
+            rateCalculator();
+
+      } else {
+            int years = 72 / userRate;
+            System.out.println("It will take " + years + " years to double your initial investment.");
+        }
+
+    }
+
+    public static void tryRateCalculator(){
+        try {
+            rateCalculator();
+        } catch (Exception e) {
+            System.out.println("Sorry. That's not a valid input.");
+            rateCalculator();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        tryRateCalculator();
+
+    }
 }
