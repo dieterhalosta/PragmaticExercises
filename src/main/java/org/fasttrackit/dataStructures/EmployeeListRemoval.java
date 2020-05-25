dfe7a1fb7b6a3df2bbd4cb9f23343300a54a7178
Dieter Halosta <dieter.halosta@yahoo.com>
2020-05-25 16:05:54 +0300

    Implemented a EmployeeListRemoval method

--- src/main/java/org/fasttrackit/dataStructures/EmployeeListRemoval.java
+++ src/main/java/org/fasttrackit/dataStructures/EmployeeListRemoval.java
@@ -1,4 +1,28 @@
 package org.fasttrackit.dataStructures;
 
+import org.fasttrackit.utils.ScannerUtils;
+
+import java.util.ArrayList;
+
 public class EmployeeListRemoval {
+    public static void main(String[] args) {
+
+        ArrayList<String> employees = new ArrayList<String>();
+        employees.add("John Smith");
+        employees.add("Jackie Jackson");
+        employees.add("Chris Jones");
+        employees.add("Amanda Cullen");
+        employees.add("Jeremy Goodwin");
+
+        System.out.println("There are " + employees.size() + " employees:\n" + employees + "\nEnter an employee name to remove:");
+        String userInput = ScannerUtils.nextLine();
+
+        for (int i=0; i<=employees.size(); i++){
+            if(userInput.equals(employees.get(i))){
+                employees.remove(i);
+                System.out.println("There are " + employees.size() + " employees:\n" + employees);
+                break;
+            }
+        }
+    }
 }
