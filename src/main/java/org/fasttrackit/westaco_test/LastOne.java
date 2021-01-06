af6d8113b3c329be5887243c79872d0a3900d65c
Dieter <dieter.halosta@yahoo.com>
2021-01-06 17:57:32 +0200

    Adding descriptions to tests

--- src/main/java/org/fasttrackit/westaco_test/LastOne.java
+++ src/main/java/org/fasttrackit/westaco_test/LastOne.java
@@ -2,6 +2,15 @@ package org.fasttrackit.westaco_test;
 import java.util.concurrent.Callable;
 
 public class LastOne {
+
+    /*
+    *   Add the missing code to Hen and Egg so the following actions are completed:
+    *  * Hen implements the Fowl interface.
+    *  * A Hen lays an egg that will hatch into a new Hen.
+    *  * Eggs from other types of fowls should hatch into a new fowl of their parent type.
+    *  * Hatching an egg for the second time throws a IllegalStateException.
+    * */
+
     interface Fowl {
         Egg lay();
     }
