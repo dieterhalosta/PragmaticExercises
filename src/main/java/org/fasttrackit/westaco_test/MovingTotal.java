6c35e5e54780430bfb99c96f33b98a39cc71f51b
Dieter <dieter.halosta@yahoo.com>
2021-01-06 17:45:48 +0200

    Adding descriptions to tests

--- src/main/java/org/fasttrackit/westaco_test/MovingTotal.java
+++ src/main/java/org/fasttrackit/westaco_test/MovingTotal.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MovingTotal {
+    /*
+    *   Design a data structure that can, efficiently with respect to time used, store and check if the total of any
+    * three successively added elements is equal to a given total.
+    *
+    *   For example, new MovingTotal() creates an empty container with no existing totals. append({1, 2, 3}) appends
+    * elements {1, 2, 3}, which means that there is only one existing total (1 + 2 + 3 = 6). append({4}) appends element
+    * 4 and creates and additional total from {2, 3, 4}. There would now be two totals (1 + 2 + 3 = 6 and 2 + 3 + 4 = 9).
+    * */
+
+
     List<Integer> numbers = new ArrayList<>();
     /**
      * Adds/appends list of integers at the end of internal list.
